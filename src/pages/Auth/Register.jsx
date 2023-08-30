c949fdf47a0b03f529b62266aca09ee4f3b3e39c
alessandro <alessandrodamata6@gmail.com>
2023-08-30 17:48:05 -0300

    Criando uma configuração de requisiçoes HTTP

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -3,19 +3,32 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setconfirmPassword] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        const user = {
+            name: name,
+            email: email,
+            password: password,
+            confirmPassword: confirmPassword
+        };
+
+        // console.log(user);
     };
     return (
         <div id="register">
             <h2>Comunica+</h2>
             <p className="subtitle">Cadastre para ver as fotos dos seus amigos.</p>
             <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="Nome" />
-                <input type="email" placeholder="E-mail" />
-                <input type="password" placeholder="Senha" />
-                <input type="password" placeholder="Confirmar senha" />
+                <input type="text" placeholder="Nome" value={name} onChange={e => setName(e.target.value)} />
+                <input type="email" placeholder="E-mail" value={email} onChange={e => setEmail(e.target.value)} />
+                <input type="password" placeholder="Senha" value={password} onChange={e => setPassword(e.target.value)} />
+                <input type="password" placeholder="Confirmar senha" value={confirmPassword} onChange={e => setconfirmPassword(e.target.value)} />
                 <input type="submit" value={"Cadastrar"}/>
             </form>
             <p>Já tem conta? <Link to={"/login"}>Clique aqui.</Link></p>
