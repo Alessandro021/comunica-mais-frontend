80e326d4b258d7b8b3a1f61c81a44a9bfce82053
alessandro <alessandrodamata6@gmail.com>
2023-08-31 16:57:41 -0300

    Finalizado a estilização da tela de login

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,9 +1,26 @@
 import "./Auth.css";
+import { Link } from "react-router-dom";
+import Message from "../../components/Message/Message";
 
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+    };
     return (
-        <div>
-            <h2>Login</h2>
+        <div id="login">
+            <h2>Comunica+</h2>
+            <p className="subtitle">Faça o login para ver o que há de novo.</p>
+            <form onSubmit={handleSubmit}>
+                <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
+                <input type="password" placeholder="Senha" value={password} onChange={e => setPassword(e.target.value)} />
+                <input type="submit" value="Entrar"  />
+            </form>
+            <p>Não tem uma conta? <Link to={"/register"}>Clique aqui.</Link></p>
         </div>
     );
 };
